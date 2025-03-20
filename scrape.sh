6fd81c6dfbf75cedf8868b5242be29f6f43df2f6
Andrew Long <longcreative@gmail.com>
2025-03-20 08:41:00 -0700

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 ./download.sh 'https://azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf'
-file="azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf"
-timestamp=$(date +%Y%m%d_%H%M%S)
-new_file_name="${timestamp}_${file}"
-mv "$file" "$new_file_name"
