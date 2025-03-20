8f8c256037e68fe8489a6bb562d9702c4e469d25
Andrew Long <longcreative@gmail.com>
2025-03-20 08:42:49 -0700

    Update timestamp.sh

--- timestamp.sh
+++ timestamp.sh
@@ -1,4 +1,5 @@
-file="azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf"
+#!/bin/bash
+file="azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf.pdf"
 timestamp=$(date +%Y%m%d_%H%M%S)
 new_file_name="${timestamp}_${file}"
 mv "$file" "$new_file_name"
