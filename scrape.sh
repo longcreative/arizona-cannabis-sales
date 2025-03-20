4d24385ac8d470582ee08b6d9671f72b579583b5
Andrew Long <longcreative@gmail.com>
2025-03-20 08:35:42 -0700

    Update scrape.sh
    fixed file name

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ./download.sh 'https://azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf'
-file="https://azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf"
+file="azdor.gov/sites/default/files/document/MJ_byPeriodCovered.pdf"
 timestamp=$(date +%Y%m%d_%H%M%S)
 new_file_name="${timestamp}_${file}"
 mv "$file" "$new_file_name"
